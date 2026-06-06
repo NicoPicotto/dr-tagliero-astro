34e520c2b21ac05bec1fcf8af604f802e7829134
Nicolas Picotto <nicopicotto@hotmail.com>
2026-06-06 13:37:40 -0300

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  site: "https://matiastagliero.com",
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
